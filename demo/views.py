b2533748bbbafad619d801b836355dbda5f7ab15
Ian Waldron <ian.waldron@scribawaldron.com>
2025-05-22 10:20:20 -0700

    add basic article list view

--- demo/views.py
+++ demo/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from core.models import Article
+
+
+class ArticleListView(ListView):
+    template_name = 'demo/article-list.html'
+    model = Article
+    # our model doesn't have default odering so apply ordering here
+    ordering = ('-pk',)
+
