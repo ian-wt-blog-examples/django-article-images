8cb04581ffb1301e5be862653dd88110f0fc9742
Ian Waldron <ian.waldron@scribawaldron.com>
2025-05-22 09:52:06 -0700

    add tag & cat to models

--- core/models.py
+++ core/models.py
@@ -3,6 +3,20 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
 class Article(models.Model):
     title = models.CharField(max_length=100)
     category = models.ForeignKey('core.Category', on_delete=models.PROTECT)
